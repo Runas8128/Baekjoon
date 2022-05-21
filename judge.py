67610189bbf6df644f063cea6252640174e4f396
Runas <minseongexcalibur@naver.com>
2022-05-21 21:00:33 +0900

    remove judge script
    Actually I don't use it :-)

--- judge.py
+++
@@ -1,7 +0,0 @@
-with open('../Working/io/correct.txt', 'r') as correctFile:
-    correct = correctFile.read().rstrip()
-
-with open('../Working/io/output.txt', 'r') as outputFile:
-    output = outputFile.read().rstrip()
-
-print('Success' if correct == output else 'Failed')
